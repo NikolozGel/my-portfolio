d3e72105768cb204728d6022df6940d554a186fb
NikolozGel <nikolozgelenidze9@gmail.com>
2025-03-31 03:44:05 +0400

    add link and target blank attribute for my mail and mobile list

--- app/components/AboutMeSection.tsx
+++ app/components/AboutMeSection.tsx
@@ -2,6 +2,7 @@
 import { Dispatch, SetStateAction, useState } from "react";
 import Image from "next/image";
 import { AnimatePresence, motion } from "framer-motion";
+import Link from "next/link";
 
 interface ITabClick {
   handleTabClick: (tab: "personal" | "professional") => void;
@@ -140,30 +141,38 @@ const AboutMeSection = ({
                 transition={{ duration: 0.4, ease: "easeInOut" }}
               >
                 <ul className="py-2 ml-5 flex flex-col gap-1">
-                  <li className="flex items-center">
-                    <Image
-                      src="/assets/contact-icons/mail-icon.png"
-                      width={10}
-                      height={10}
-                      alt="mail"
-                      className="xl:hidden"
-                    />
-                    <p className="text-[#607b96] font-semibold ml-6 mb-1 break-words">
-                      nikolozgelenidze9@gmail.com
-                    </p>
-                  </li>
-                  <li className="flex items-center">
-                    <Image
-                      src="/assets/contact-icons/phone-icon.png"
-                      width={10}
-                      height={10}
-                      alt="phone"
-                      className="mr-5"
-                    />
-                    <p className="text-[#607b96] font-semibold break-words">
-                      +(995) 511 10 60 81
-                    </p>
-                  </li>
+                  <Link
+                    href={"mailto:nikolozgelenidze9@gmail.com"}
+                    target="_blank"
+                  >
+                    <li className="flex items-center">
+                      <Image
+                        src="/assets/contact-icons/mail-icon.png"
+                        width={10}
+                        height={10}
+                        alt="mail"
+                        className="xl:hidden"
+                      />
+                      <p className="text-[#607b96] font-semibold ml-6 mb-1 break-words">
+                        nikolozgelenidze9@gmail.com
+                      </p>
+                    </li>
+                  </Link>
+
+                  <Link href={"tel:+995511106081"} target="_blank">
+                    <li className="flex items-center">
+                      <Image
+                        src="/assets/contact-icons/phone-icon.png"
+                        width={10}
+                        height={10}
+                        alt="phone"
+                        className="mr-5"
+                      />
+                      <p className="text-[#607b96] font-semibold break-words">
+                        +(995) 511 10 60 81
+                      </p>
+                    </li>
+                  </Link>
                 </ul>
               </motion.div>
             )}
