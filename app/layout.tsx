a3be9df3f285c8b9d134201b40b76848a9720ac5
NikolozGel <nikolozgelenidze9@gmail.com>
2025-04-12 15:09:31 +0400

    sss

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Fira_Code, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Footer from "./components/Yleoba-Footer";
 
 const FiraCode = Fira_Code({
   subsets: ["latin"],
