27c518dc8a37fb700bccc48ea592360f7cb7e084
NikolozGel <nikolozgelenidze9@gmail.com>
2025-04-12 15:05:37 +0400

    temp name Footer and Header

--- app/components/NavigationMenu.tsx
+++ app/components/NavigationMenu.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Footer from "./Footer";
+import Footer from "./Footer1";
 import { AnimatePresence, motion } from "framer-motion";
 import { Dispatch, SetStateAction } from "react";
 
