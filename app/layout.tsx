ba022ae20c711988fed10675e77127fde0df862d
NikolozGel <nikolozgelenidze9@gmail.com>
2025-04-12 15:10:26 +0400

    change name footer and header components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Fira_Code, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Yleoba-Footer";
+import Header from "./components/Yleoba-header";
+import Footer from "./components/Footer";
 
 const FiraCode = Fira_Code({
   subsets: ["latin"],
